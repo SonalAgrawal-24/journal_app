caa734d3673c1b49eece2ac874f4989dee6b7efb
Sonal Agrawal <agrawalsonal0624@gmail.com>
2024-08-12 11:24:01 +0530

    Introduction to Mockito

---
+++ src/test/java/com/sonal/journalApp/UserDetailsServiceImplTests.java
@@ -0,0 +1,37 @@
+package com.sonal.journalApp;
+
+import com.sonal.journalApp.entity.User;
+import com.sonal.journalApp.repository.UserRepository;
+import com.sonal.journalApp.service.UserDetailServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+//@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserDetailsServiceImplTests {
+
+//    @Autowired
+    @InjectMocks
+    private UserDetailServiceImpl userDetailServiceImpl;
+
+//    @MockBean
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void loadUSerByUsernameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("wifulb").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailServiceImpl.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
