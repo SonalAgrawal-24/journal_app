ceb0ee658d71ee58645c87a94db314cfe08132a4
SonalAgrawal-24 <agrawalsonal0624@gmail.com>
2024-08-22 10:08:15 +0530

    Replaced hardcoding of values and therfore used "value" annotation

--- src/main/java/com/sonal/journalApp/service/WeatherService.java
+++ src/main/java/com/sonal/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.sonal.journalApp.service;
 
 import com.sonal.journalApp.api.Response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class WeatherService {
 
-    private static final String apiKey = "b893359d38c4ef0807e7c75503e734c1";
+    @Value("${weather_api_key}")
+    private String apiKey;
 
     public final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
