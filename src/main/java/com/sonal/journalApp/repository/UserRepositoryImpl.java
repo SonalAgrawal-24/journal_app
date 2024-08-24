e993dbe0681a606371d0c872b1821e3e0d7037fc
SonalAgrawal-24 <agrawalsonal0624@gmail.com>
2024-08-24 18:37:48 +0530

    Added mail and setimentAnalysis field in db and used Query and Criteria to fetch data from db

---
+++ src/main/java/com/sonal/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.sonal.journalApp.repository;
+
+import com.sonal.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUsersforSA(){
+        Query query = new Query();
+//        query.addCriteria(Criteria.where("email").exists(true));
+//        query.addCriteria(Criteria.where("email").ne(null).ne(""));
+//        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+
+//        using and/or operator for criterias
+//        Criteria criteria = new Criteria();
+//        query.addCriteria(criteria.orOperator(
+//                Criteria.where("email").exists(true),
+//                Criteria.where("sentimentAnalysis").is(true))
+//        );
+
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-z|a-z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        query.addCriteria(Criteria.where("roles").in("USER", "ADMIN")); //to play around multiple options of a variable, since roles is an array
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
+}
